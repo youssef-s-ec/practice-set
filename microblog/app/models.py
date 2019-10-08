11fa57cb4d727a6a3bfe84b28a8d74b1897d03ae
youssef-passnfly <youssef@sast-mfa.com>
2019-10-08 18:06:18 +0200

    chapter4 completed and working good

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic') #### causing error when adding user on python terminal
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
