fe3aea753dabb42a41cb12ebb389e6858857c308
youssef-passnfly <youssef@sast-mfa.com>
2019-10-15 14:24:57 +0200

    chapter4 completed and working good

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,12 +1,10 @@
 from datetime import datetime
-from app import db
-from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
 from flask_login import UserMixin
-from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -23,6 +21,11 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
 
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -31,10 +34,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Posts {}>'.format(self.body)
-
-
-class User(UserMixin, db.Model):
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
