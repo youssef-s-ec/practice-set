f32d55bd7739dc4fc985579924a714c2d50d9bf7
youssef-development <youssef.s.ec@gmail.com>
2019-10-21 17:57:38 +0200

    Chapter 7 completed, but the raise error message showing an empty brackets only, need to double check it

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -7,6 +7,13 @@ from app.models import User
 from datetime import datetime
 
 
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -70,21 +77,15 @@ def user(username):
     posts = [
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'},
-        {'author': user, 'body': 'hola amigos'},
+        {'author': user, 'body': 'hola amigos'}
     ]
     return render_template('user.html', user=user, posts=posts)
 
 
-@app.before_request
-def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
-
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
@@ -94,4 +95,4 @@ def edit_profile():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return  render_template('edit_profile.html', title='Edit Profile', form=form)
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
